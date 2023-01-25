f2bb56f6fce0f89a98275c5e992f1be711a15d90
VMantsevich <vladislava_mantsevich@mail.ru>
2023-01-25 23:09:54 +0300

    Initial commit

---
+++ src/main/java/ru/netology/oop/FormDate.java
@@ -0,0 +1,7 @@
+package ru.netology.oop;
+
+public class FormDate {
+    public int day;
+    public int month;
+    public int year;
+}
---
+++ src/main/java/ru/netology/oop/Post.java
@@ -0,0 +1,14 @@
+package ru.netology.oop;
+
+public class Post {
+    public FormDate birthday;
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+
+
+
+}
